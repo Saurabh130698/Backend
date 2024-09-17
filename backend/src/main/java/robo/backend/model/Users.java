d5fe3cea7340a88a3f4e422c80b5c0f263dfceaa
UtkarshGhadage <utkarshghadage01@gmail.com>
2024-09-18 01:41:22 +0530

    basic auth

---
+++ backend/src/main/java/robo/backend/model/Users.java
@@ -0,0 +1,18 @@
+package robo.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Users {
+    @Id
+    long id;
+    String username;
+    String email;
+    String password;
+}
